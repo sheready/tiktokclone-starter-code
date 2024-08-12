cfc950d81e3a034e59a3f378182acc64fbab1519
sheready <csstlw@gmail.com>
2024-08-12 13:11:09 +0300

    change background color

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
-            <input type="search" id="search" class="block w-[30vw] p-2.5 text-md text-gray-200 border rounded-3xl bg-slate-800" placeholder="Search"/>
+            <input type="search" id="search" class="block w-[30vw] p-2.5 text-md text-gray-200 border rounded-3xl bg-lightGrey" placeholder="Search"/>
         </div>
       </div>
       <div className='flex flex-row justify-end'>
