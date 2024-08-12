cd8ec430881c5ae0ae6d7d1310e70b3889221a3c
sheready <csstlw@gmail.com>
2024-08-12 13:12:58 +0300

    streamline layout page

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -10,10 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+        <link rel="icon" href="/logo.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/logo.png" sizes="180x180" />
+      </head>
       <body>
         <Navbar/>
-        {children}
+        <main className="relative overflow-hidden">   
+          {children}
+        </main>
       </body>
     </html>
   );
