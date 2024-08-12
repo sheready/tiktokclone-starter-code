a71fb2a7d1b3c4700d122da15ba35418cf8a6933
sheready <csstlw@gmail.com>
2024-08-12 13:13:32 +0300

    add and edit columns to the page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
+import LeftBar from "./homepage/LeftBar";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-row w-[100%]">
+      <div className="w-[15%]">
+        <LeftBar />
+      </div>
+      <div className="w-[70%]">
+
+      </div>
+      <div className="w-[15%]">
+        
+      </div>
       
     </div>
 
