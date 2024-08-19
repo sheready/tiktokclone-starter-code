3c97e110f09c83d1891b210782c8f7777264904c
sheready <csstlw@gmail.com>
2024-08-19 09:24:02 +0300

    add feed js code

--- app/homepage/Feed.js
+++ app/homepage/Feed.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className='flex justify-center '>
+      <div className='rounded-2xl px-12 w-[500px] h-full'>
+        <img src="https://images.pexels.com/photos/1631678/pexels-photo-1631678.jpeg?cs=srgb&dl=pexels-abdghat-1631678.jpg&fm=jpg"></img>
+      </div>
+    </div>
   )
 }
 
